38862e47bc0ca2752d4832d352aaf859613bb5f1
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-03-05 22:18:16 +0600

    Added all files

---
+++ 05.chapter-five/30.string.js
@@ -0,0 +1,22 @@
+var str1 = 'something';
+console.log(str1);
+
+var str2 = String(10);
+console.log(str2);
+
+var n = 10;
+console.log(n);
+
+var str3 = n.toString();
+console.log(str3);
+
+// String Notation
+var str4 = 'What\'s up "man"?';
+console.log(str4);
+console.log('This is a \nnew line');
+console.log('This is \t Tab');
+
+// String Comparison
+console.log('z' > 'Z');
+console.log('z' > 'a');
+// z>>>>>>a>>>>>Z>>>>>A
---
+++ 05.chapter-five/31.string-methods.js
@@ -0,0 +1,32 @@
+// Concat
+var a = 'I';
+var b = ' am';
+var c = ' Faysal';
+var d = a.concat(b, c);
+console.log(d);
+
+// substr
+var e = d.substr(5, 4);
+console.log(e);
+
+// charAt
+var f = d.charAt(5);
+console.log(f);
+
+// startsWith,endsWith
+console.log(d.startsWith('I'));
+console.log(d.endsWith('Faysal'));
+
+// upperCase(),lowerCase
+console.log('hi'.toUpperCase());
+console.log('HI'.toLowerCase());
+
+// Trim()
+console.log('    hi'.trim());
+
+// split
+console.log(d.split(' '));
+
+// length
+var name = 'Faysal';
+console.log(name.length);
