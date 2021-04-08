b6587dd1f7f1ed6d867da13287d92fe521dfa29a
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-04-08 14:25:19 +0600

    Added all files

--- 15.chapter-fifteen/79.arrow-function.js
+++ 15.chapter-fifteen/79.arrow-function.js
@@ -5,7 +5,7 @@ let myFunc = () => {
 
 myFunc();
 
-// Arrow Function Doesn't Care About 'this
+// Arrow Function Doesn't Care About 'this'
 let javascript = {
   name: 'Javascript',
   libraries: ['React', 'Vue', 'Angular'],
