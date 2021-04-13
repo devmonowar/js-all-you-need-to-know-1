7f620a7edb0a2a71a38b6d4f60fe11a8c1be22a5
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-04-14 00:39:38 +0600

    added all files

--- 18.chapter-eighteen/10.checkbox-handling.js
+++ 18.chapter-eighteen/10.checkbox-handling.js
@@ -1,5 +1,6 @@
 const skill = document.getElementsByName('skill');
 const skillOutput = document.querySelector('.skills-output');
+const result = document.querySelector('#result');
 
 const checkedSkills = [];
 
@@ -7,11 +8,19 @@ const checkedSkills = [];
   value.addEventListener('change', function (event) {
     if (event.target.checked) {
       checkedSkills.push(event.target.value);
-      console.log(checkedSkills);
+      showedSkills(result, checkedSkills);
     } else {
       let index = checkedSkills.indexOf(event.target.value);
       checkedSkills.splice(index, 1);
-      console.log(checkedSkills);
+      showedSkills(result, checkedSkills);
     }
   });
 });
+
+function showedSkills(parent, skills) {
+  let result = '';
+  skills.forEach((value, index) => {
+    result += `(${index + 1}) ${value} ,  `;
+  });
+  parent.innerHTML = result;
+}
