d935860f5e77709d86c0aeb22b5d8dc60c226ff0
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-04-14 03:11:21 +0600

    added all files

--- 18.chapter-eighteen/12.form-handling.js
+++ 18.chapter-eighteen/12.form-handling.js
@@ -12,37 +12,14 @@ form.addEventListener('submit', function (event) {
   // traverse element
   elements.forEach((element) => {
     if (element.type !== 'submit') {
-      if (element.value.trim() === '') {
-        // Validation
-        goodFeedback('d-none', 'd-block');
-        errorFeedback('d-block', 'd-none');
-      } else {
-        // set data
-        data[element.name] = element.value;
-        // Validation
-        goodFeedback('d-block', 'd-none');
-        errorFeedback('d-none', 'd-block');
-      }
+      // set data
+      data[element.name] = element.value;
     }
   });
+
   // log data
   console.log(data);
+
   // reset inputs
   this.reset();
 });
-
-// good feedback function
-function goodFeedback(addClass, removeClass) {
-  document.querySelectorAll('.good-feedback').forEach((value) => {
-    value.classList.add(addClass);
-    value.classList.remove(removeClass);
-  });
-}
-
-// error feedback function
-function errorFeedback(addClass, removeClass) {
-  document.querySelectorAll('.error-feedback').forEach((value) => {
-    value.classList.add(addClass);
-    value.classList.remove(removeClass);
-  });
-}
